56f1d09e011401660046010c78a18463c9057fd5
qw753954 <68770475+qw753954@users.noreply.github.com>
2021-07-12 23:27:36 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:qw753954/vue-dorii.git master:gh-pages
+git push -f https://github.com/qw753954/vue-dorii.git master:gh-pages
 
 cd -
